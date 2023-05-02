28cdfdde97683aaaf46fc08446241638a5671cf7
HenuZiO <mshenuz@gmail.com>
2023-05-02 23:13:32 +0300

    End of lesson 7

---
+++ assets/js/lesson-7.js
@@ -0,0 +1,13 @@
+const outputField = document.getElementById('output-field');
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = 'A String';
+titleCaseOver = 9000;
+
+outputField.innerText = `${studlyCapVar}, ${properCamelCase}, ${titleCaseOver}`;
