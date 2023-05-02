23e25f37721731ee2fbb6c6e4ffbc23f2483ba25
HenuZiO <mshenuz@gmail.com>
2023-05-02 23:23:00 +0300

    end of lesson 9

---
+++ assets/js/lesson-9.js
@@ -0,0 +1,8 @@
+const outputField = document.getElementById('output-field');
+
+const FCC = 'freeCodeCamp'; // Change this line
+let fact = 'is cool!'; // Change this line
+fact = 'is awesome!';
+console.log(FCC, fact); // Change this line
+
+outputField.innerText = `${FCC}, ${fact}`;
